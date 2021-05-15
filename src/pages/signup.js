7144dab5cfb51b98cec10d348cef0c3fe7346e91
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-05-15 14:48:55 -0400

    updating signup page with components

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
-<<<<<<< HEAD
-import SignUp from "../components/SignUpForm";
-import SignUpimg from "../components/signUpimg";
-=======
->>>>>>> origin/main
+import {Input, LabelForInput, Image} from "../components/Form";
+import {Buttons} from "../components/Buttons";
+import GenericImage from "../images/styleMe.png";
+import { Button } from "bootstrap";
+import "../styles/styles.css"
 
 function Signup() {
   return (
     <>
       <Link to={"/signup"}style={{textDecoration:"none"}}>
         <Header/>
+      <Image src ={GenericImage} alt= "Style Me" className= "style_me_logo"/>
+      <LabelForInput htmlFor = "first_name"/>
+      <Input type="text"
+          id="first_name"
+          className = "whatever"
+          placeholder="First Name"
+          name="first_name"
+          required=""/>
+          {/* <Input type="text"
+          id="first_name"
+          placeholder="First Name"
+          name="first_name"
+          required=""/> */}
+      {/* <Buttons onClick="">Sign Me Up</Buttons> */}
+      <Buttons>Sign Me Up</Buttons>
+      <Buttons to = "/home">Cancel</Buttons>
         
       </Link>
-      <SignUp/>
     </>
   );
 }
