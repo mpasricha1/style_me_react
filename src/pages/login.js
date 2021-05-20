1bd230adc6349b9c81927cdbc8d7eb56788fe1bf
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2021-05-20 15:53:00 -0400

    Added sinitial code to return login data

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useContext, useRef} from "react";
 import Header from "../components/Header";
-import { Input, Image } from "../components/Form";
+import { Image } from "../components/Form";
 import { Buttons } from "../components/Buttons";
 import GenericImage from "../images/styleMe.png";
 import Footer from "../components/Footer";
 import "../styles/styles.css";
+import API from "../utils/API";
 
 function Login() {
+
+   // **************** TEST ****************
+  //  const { id, setId, name, setName, loggedIn, setLoggedIn } =
+  //  useContext(UserContext);
+ const emailInput = useRef();
+ const passwordInput = useRef();
+
+ const handleSubmit = (event) => {
+   // if the user hits enter or hits the button, this function will fire
+   event.preventDefault();
+   console.log({
+     email: emailInput.current.value,
+     password: passwordInput.current.value,
+   });
+   
+   API.testUserRouter({
+     email: emailInput.current.value,
+     password: passwordInput.current.value,
+   })
+     .then((data) => {
+       console.log(data);
+       
+     })
+     .catch((err) => console.log(err));
+ };
   return (
     <>
       <Header />
@@ -14,16 +40,18 @@ function Login() {
 
       <form>
         <div className="form-floating mb-3 input_field">
-          <Input
-            type="text"
+        <input
+            ref={emailInput}
+            type="email"
             id="email"
-            className="inputBox firstInputBox"
+            className="inputBox"
             placeholder="Email"
             name="email"
             required=""
           />
 
-          <Input
+          <input
+            ref={passwordInput}
             type="password"
             id="password"
             className="inputBox"
@@ -33,9 +61,15 @@ function Login() {
           />
         </div>
 
-        {/* <Buttons onClick="******">Login</Buttons> */}
-        <Buttons>Login</Buttons>
-        {/* <Buttons on click= "*****">Google</Buttons> */}
+        <div className="landing_btn_container">
+          <button
+            type="submit"
+            onClick={handleSubmit}
+            className="btn btn-outline-secondary signmeUpBtn"
+          >
+            Login
+          </button>
+        </div>
         <Buttons>Google</Buttons>
         {/* <Buttons to = "/home">Cancel</Buttons> */}
       </form>
