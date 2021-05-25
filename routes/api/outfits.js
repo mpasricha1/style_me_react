26617f32ae855d8f1c7687b87bc9f0e32e435776
Mark Pasricha <markp862@gmail.com>
2021-05-24 21:58:21 -0400

    adding routes

--- routes/api/outfits.js
+++ routes/api/outfits.js
@@ -3,5 +3,9 @@ const outfitController = require("../../controllers/outfitController");
 
 router.route('/')
 	.get(outfitController.getOutfits)
+	.post(outfitController.addOutfit)
+
+router.route('/item')
+	.post(outfitController.addOutfitItem)
 
 module.exports = router; 
