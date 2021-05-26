18bdef878afbb99b3b574ff6e20a06a798b44518
jennifermendez <jjael8919@gmail.com>
2021-05-25 22:41:27 -0400

    social page

--- client/src/pages/social.js
+++ client/src/pages/social.js
@@ -14,155 +14,161 @@ import Footer from "../components/Footer";
 function Social() {
   return (
     <>
-        <Header />
-        <div className="socialtopButtons">
-          <SocialBtn
-            to="/newitem"
-            className="socialoutfitsBtn btn btn-outline-secondary"
-          >
-            Build Outfits
-          </SocialBtn>
-          <SocialBtn
-            to="/buildoutfit"
-            className="shareoutfitBtn btn btn-outline-secondary"
-          >
-            Share Outfits
-          </SocialBtn>
-        </div>
+      <Header />
+      <div className="socialtopButtons">
+        <SocialBtn
+          to="/newitem"
+          className="socialoutfitsBtn btn btn-outline-secondary"
+        >
+          Build Outfits
+        </SocialBtn>
+        <SocialBtn
+          to="/buildoutfit"
+          className="shareoutfitBtn btn btn-outline-secondary"
+        >
+          Share Outfits
+        </SocialBtn>
+      </div>
 
-        <ListContainer className="socialContainer">
-          <UnorderedList className="socialUnorderList">
-            <div className="socialUserInfo">
-              <p className="socialUsername">Outfit Name</p>
-            </div>
-            <ListElement className="socialList">
-              <ImgTag
-                src={TemplateImage}
-                className="socialImg"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-            </ListElement>
-          </UnorderedList>
-
-          <UnorderedList className="socialUnorderList">
-            <div className="socialUserInfo">
-              <p className="socialUsername">Outfit Name</p>
-            </div>
-            <ListElement className="socialList">
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-            </ListElement>
-          </UnorderedList>
-
-          <UnorderedList className="socialUnorderList">
-            <div className="socialUserInfo">
-              <p className="socialUsername">Outfit Name</p>
-            </div>
-            <ListElement className="socialList">
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-            </ListElement>
-          </UnorderedList>
-
-          <ListContainer className="socialOutfit">
+      <ListContainer className="socialContainer">
+        <UnorderedList className="socialUnorderList">
           <SocialBtn
             to="/buildoutfit"
             className="postBtn btn btn-outline-secondary"
           >
             POST
           </SocialBtn>
+
+          <div className="socialUserInfo">
+            <p className="socialUsername">Stephanie Carattini</p>
+          </div>
+          <div className="dropDownSocial">
+            <button
+              className="public btn btn-secondary dropdown-toggle"
+              type="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Public
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item">Public</a>
+              <a className="dropdown-item">Friends</a>
+              <a className="dropdown-item">Only me</a>
+            </div>
+          </div>
+
+          <ListElement className="socialList">
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+          </ListElement>
+        </UnorderedList>
+        <UnorderedList className="socialUnorderList">
+          <SocialBtn
+            to="/buildoutfit"
+            className="postBtn btn btn-outline-secondary"
+          >
+            POST
+          </SocialBtn>
+
+          <div className="socialUserInfo">
+            <p className="socialUsername">Stephanie Carattini</p>
+          </div>
+          <div className="dropDownSocial">
+            <button
+              className="public btn btn-secondary dropdown-toggle"
+              type="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Public
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item">Public</a>
+              <a className="dropdown-item">Friends</a>
+              <a className="dropdown-item">Only me</a>
+            </div>
+          </div>
+
+          <ListElement className="socialList">
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+          </ListElement>
+        </UnorderedList>
+
+        <ListContainer className="socialOutfit">
           <div className="socialShareInfo">
-              <p className="socialUsername">Stephanie Carattini</p>
-              <p className="saySomething">say something about this outfit.......</p>
-            </div>
-            <UnorderedList className="socialSharedList">
-              <ListElement className="socialshaaredLi">
-              <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-               <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-               <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-               <ImgTag
-                src={TemplateImage}
-                className="imgElement"
-                placeholder="Outfits"
-              />
-              </ListElement>
-            </UnorderedList>
+            <p className="socialUsername">Stephanie Carattini</p>
+            <p className="saySomething">
+              shared a new Outfit
+            </p>
+          </div>
+         
+          <UnorderedList className="socialSharedList">
+           
+          <ListElement className="socialList">
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+            <ImgTag
+              src={TemplateImage}
+              className="socialImg"
+              placeholder="Outfits"
+            />
+          </ListElement>
+          </UnorderedList>
 
-            <div className="inputItemName">
-              <Input
-                // onChange={handleInputChange}
-                id="item_name"
-                className="socialItemName"
-                name="Item Name"
-                placeholder="comment...."
-              />
-            </div>
-
+          <div className="inputItemName">
             <Input
               // onChange={handleInputChange}
               id="item_name"
@@ -170,37 +176,29 @@ function Social() {
               name="Item Name"
               placeholder="comment...."
             />
+          </div>
 
-            <Input
-              // onChange={handleInputChange}
-              id="item_name"
-              className="socialItemName"
-              name="Item Name"
-              placeholder="comment...."
-            />
+          <Input
+            // onChange={handleInputChange}
+            id="item_name"
+            className="socialItemName"
+            name="Item Name"
+            placeholder="comment...."
+          />
 
-            <Input
-              // onChange={handleInputChange}
-              id="item_name"
-              className="socialItemName"
-              name="Item Name"
-              placeholder="comment...."
-            />
-
-            <Input
-              // onChange={handleInputChange}
-              id="item_name"
-              className="socialItemName"
-              name="Item Name"
-              placeholder="comment...."
-            />
-          </ListContainer>
+          <Input
+            // onChange={handleInputChange}
+            id="item_name"
+            className="socialItemName"
+            name="Item Name"
+            placeholder="comment...."
+          />
         </ListContainer>
+      </ListContainer>
 
-        <Footer />
+      <Footer />
     </>
   );
 }
 
 export default Social;
-
